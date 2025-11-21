763d250d05da0b520463f875a9725eda9d0752f9
hussain3139 <thelegendary313.hus@gmail.com>
2025-11-21 06:35:02 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool Is_nigga = false;
+
+
+int main(){
+
+    printf("hello nigger\n");
+
+if(Is_nigga){
+
+    printf("get this nigga bitch off the plane\n");
+
+    }
+
+else{
+    printf("are u a jew\n");
+
+}
+
+//this is tuff
+}
